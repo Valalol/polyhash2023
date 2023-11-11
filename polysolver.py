6bc7246185672d8ca8900470df64ee0590426462
Valentin Esnault <valentin.esnault@etu.univ-nantes.fr>
2023-11-11 17:04:16 +0100

    working solve strategy 0

--- polysolver.py
+++ polysolver.py
@@ -8,6 +8,25 @@ from classes import *
 
 
 def solve(challenge_data: list, solve_strategy: int = 1):
+    """
+    Solves the given challenge data using the specified strategy.
+    
+    Args:
+    - challenge_data (list): A list containing the following challenge data:
+        - rows (int): The number of rows in the grid.
+        - columns (int): The number of columns in the grid.
+        - drone_count (int): The number of drones available.
+        - deadline (int): The number of turns available.
+        - max_load (int): The maximum weight that a drone can carry.
+        - products_weight (list): A list containing the weight of each product type.
+        - warehouses_dict (dict): A dictionary containing the warehouses information.
+        - orders_dict (dict): A dictionary containing the orders information.
+    - solve_strategy (int): An integer representing the strategy to use for solving the challenge data.
+    
+    Returns:
+    - solution (list): A list containing the solution to the challenge data.
+    """
+def solve(challenge_data: list, solve_strategy: int = 1):
     rows, columns, drone_count, deadline, max_load, products_weight, warehouses_dict, orders_dict = challenge_data
     
     if solve_strategy == 0:
@@ -16,20 +35,55 @@ def solve(challenge_data: list, solve_strategy: int = 1):
         tick = 0
         order_index = 0
         orders = list(orders_dict.values())
-        
         drone = Drone(list(warehouses_dict.keys())[0], max_load, products_weight)
         
-        while tick < deadline and order_index < len(orders_dict):
+        while tick < deadline and order_index < len(orders):
+            product_index = 0
             order = orders[order_index]
-            product_type = order.items[0]
-            # for product_type in order.items:
-            drone.load({product_type: 1})
-            print(f"Loaded product {product_type} at tick {tick}")
-            # drone.deliver(order, product_type, 1)
-            # drone.tick()
-            # tick += 1
-        
-        
+            
+            state = 2
+            
+            while tick < deadline and product_index < len(order.items):
+                product_type = order.items[product_index]
+                
+                drone.tick()
+                tick += 1
+                
+                if drone.drone_busy():
+                    continue
+                    
+                
+                if state == 3:
+                    drone.load({product_type: 1})
+                    print(f"Loaded product {product_type} at tick {tick}")
+                    state = 0   
+                
+                elif state == 0:
+                    drone.travel(order.coordinates)
+                    print(f"Started travelling with product index {product_index} for order {order_index} at tick {tick}")
+                    state = 1
+                
+                elif state == 1:
+                    drone.unload({product_type: 1})
+                    print(f"Unloaded product {product_type} at tick {tick}")
+                    product_index += 1
+                    order.items.remove(product_type)
+                    if len(order.items) == 0:
+                        order_index += 1
+                        order = orders[order_index]
+                        print(f"Order {order_index} completed at tick {tick}")
+                    state = 2
+                
+                elif state == 2:
+                    for warehouse in warehouses_dict.values():
+                        if warehouse.products_info[product_type] > 0:
+                            warehouse_coordinates = warehouse.coordinates
+                            break
+                    drone.travel(warehouse_coordinates)
+                    print(f"Started travelling to warehouse ({warehouse_coordinates}) at tick {tick}")
+                    state = 3
+    
+    
     
     
     if solve_strategy == 1:
