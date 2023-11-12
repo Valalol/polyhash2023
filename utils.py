9f335bc284d6927baaf7ad353c0f6f7f9b67b996
Robinson Roué <robinson.roue@etu.univ-nantes.fr>
2023-11-12 20:01:27 +0100

    new function: check_a_in_b for dict/list

--- utils.py
+++ utils.py
@@ -13,3 +13,16 @@ def find_nearest_warehouse(coordinates: tuple[int,int], warehouses_dict: dict[tu
             closest_warehouse = warehouse
             
     return closest_warehouse
+
+def check_b_in_a(container1: list | dict, container2: list | dict):
+    retour: bool = True
+    if container2 is dict:
+        for product_type, product_number in container2.items():
+            if container1[product_type] < product_number:
+                retour = False
+    else:
+        for product in container2:
+            if container1[product_type] < 1:
+                retour = False
+    return retour
+        
