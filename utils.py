aad0a862bb61ba3cc864b255b6e0446ee085dfb2
Robinson Roué <robinson.roue@etu.univ-nantes.fr>
2023-11-12 11:34:50 +0100

    correcting utils.find_nearest_warehouse errors

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
-def find_nearest_warehouse(coordinates, warehouses_dict):
+from classes import *
+
+def find_nearest_warehouse(coordinates: tuple[int,int], warehouses_dict: dict[tuple[int,int], Warehouse], max_dist: int) -> Warehouse | None:
     
     min_dist: int = max_dist
     closest_warehouse: Warehouse | None = None
     
-    for warehouse in warehouse_dict.values():
+    for warehouse in warehouses_dict.values():
         
         distance: int = dist(coordinates, warehouse.coordinates)
         if distance < min_dist:
