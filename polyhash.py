b9fff88a110ed007578edeb89e1574ba5474b422
Valentin Esnault <valentin.esnault@etu.univ-nantes.fr>
2024-01-19 10:54:34 +0100

    upgrade to stratgy3 : drones can now pick up items for multiple orders from the same warehouse

--- polyhash.py
+++ polyhash.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     # c_redudancy
     # d_mother_of_all_warehouses
     if debug:
-        args = type("Args", (object,), {"challenge": r"challenges\d_mother_of_all_warehouses.in", "output": "output/out.txt"})
+        args = type("Args", (object,), {"challenge": r"challenges\c_redudancy.in", "output": "output/out.txt"})
     else:
         import argparse
 
--- strategies/strategy_3.py
+++ strategies/strategy_3.py
@@ -48,42 +48,66 @@ def solve(challenge_data):
                         selected_warehouse = warehouse
                         break
 
-                interesting_items = []
-                order_remaining_items_hist = {}
-                for item in order.remaining_items:
-                    if item in order_remaining_items_hist:
-                        order_remaining_items_hist[item] += 1
-                    else:
-                        order_remaining_items_hist[item] = 1
-
-                for item in order_remaining_items_hist:
-                    if selected_warehouse.predicted_products_info[item] > 0:
-                        amount = min(selected_warehouse.predicted_products_info[item], order_remaining_items_hist[item])
-                        for _ in range(amount):
-                            interesting_items.append(item)
-
+                can_still_load_items = True
                 products = {}
-                item_index = 0
-                product = interesting_items[item_index]
-                while calculate_weigth(products, products_weight) + products_weight[product] < max_load and item_index <= len(interesting_items) - 1:
-                    if product in products:
-                        products[product] += 1
-                    else:
-                        products[product] = 1
+                while can_still_load_items and order_index < len(orders):
+                    if len(order.remaining_items) == 0:
+                        if debug:
+                            print(f"All items of order {order_index} are atributed (tick : {tick.value})")
+                        order_index += 1
+                        if order_index >= len(orders):
+                            continue
+                        order = orders[order_index]
+                    
+                    interesting_items = []
+                    order_remaining_items_hist = {}
+                    for item in order.remaining_items:
+                        if item in order_remaining_items_hist:
+                            order_remaining_items_hist[item] += 1
+                        else:
+                            order_remaining_items_hist[item] = 1
 
-                    item_index += 1
-                    if item_index <= len(interesting_items) - 1:
+                    for item in order_remaining_items_hist:
+                        if item in products:
+                            available = selected_warehouse.predicted_products_info[item] - products[item]
+                        else:
+                            available = selected_warehouse.predicted_products_info[item]
+
+                        if available > 0:
+                            amount = min(available, order_remaining_items_hist[item])
+                            for _ in range(amount):
+                                interesting_items.append(item)
+
+                    if len(interesting_items) == 0:
+                        break
+                    else:
+                        item_index = 0
                         product = interesting_items[item_index]
+                        can_still_load_items = calculate_weigth(products, products_weight) + products_weight[product] < max_load
+                        take_more_items = can_still_load_items and item_index <= len(interesting_items) - 1
+                        while take_more_items:
+                            if product in products:
+                                products[product] += 1
+                            else:
+                                products[product] = 1
+                            
+                            order.remaining_items.remove(product)
+                            if debug:
+                                print(f"Started loading product {product} from warehouse {selected_warehouse.warehouse_id} at tick {tick.value} by drone {i} for order {order.order_id}")
+                            solution += f"{i} L {selected_warehouse.warehouse_id} {product} 1\n"
+
+                            drone.orders_memory.append([order, product])
+
+                            item_index += 1
+                            if item_index <= len(interesting_items) - 1:
+                                product = interesting_items[item_index]
+                                can_still_load_items = calculate_weigth(products, products_weight) + products_weight[product] < max_load
+                                take_more_items = can_still_load_items
+                            else:
+                                can_still_load_items = True
+                                take_more_items = False
 
                 drone.load(products, selected_warehouse)
-                drone.current_order = order
-                for product_type in products:
-                    for _ in range(products[product_type]):
-                        order.remaining_items.remove(product_type)
-                        if debug:
-                            print(f"Started loading product {product_type} from warehouse {selected_warehouse.warehouse_id} at tick {tick.value} by drone {i} for order {order.order_id}")
-                        solution += f"{i} L {selected_warehouse.warehouse_id} {product_type} 1\n"
-                        drone.orders_memory.append([order, product_type])
                 drone.state = 1
 
             elif drone.state == 1:
